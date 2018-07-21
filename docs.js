cebbe83a8eac5f7d1a6dcc6694573801dbb0f367
Gustavo <gustavomaritan@gmail.com>
2018-07-21 14:40:38 -0300

    ddd

---
+++ docs.js
@@ -0,0 +1,147 @@
+let doc = {
+    api: {
+        name: 'Payment',
+        url: { /* Parãmetro no commander para definir qual ambiente usar */
+            dev: 'http://localhost:11002',
+            homolog: '',
+            prod: ''
+        },
+        prefix: 'api',
+        headers: { authorization: { type: String, descricao: 'Chave gerada pelo admin', testValue: '123' } }
+    },
+    controllers: [
+        {
+            name: 'Boletos',
+            descricao: 'Métodos para geração de boletos',
+            routes: [
+                {
+                    name: 'Gerar Boleto',
+                    descricao: 'Método responsavel por gerar boleto e retornar url para impressão ou download.',
+                    method: 'POST',
+                    url: 'boleto/:banco',
+                    //headers: { authorization: '' },
+                    params: {
+                        banco: { required: true, opcoes: ['brasil', 'sicredi', 'sicoob'], type: String, testValue: 'brasil' },
+                        t: { default: 'print', opcoes: ['print', 'download'], type: String }
+                    },
+                    //body:{},
+                    bodyCase: {
+                        from: 'banco', // PARAMETRO CASE
+                        itens: [
+                            {
+                                name: 'brasil',
+                                value: {
+                                    dataImpressao: { type: Date, descricao: 'Data impressão do boleto' },
+                                    dataGeracaoBoleto: Date,
+                                    numeroDocumento: String,
+                                    especie: String,
+                                    carteira: String,
+                                    numeroConvenio: String,
+                                    pagador: {
+                                        nome: String,
+                                        edereco: String,
+                                        cep: String,
+                                        cpfCnpj: String,
+                                    },
+                                    beneficiario: {
+                                        nome: String,
+                                        cpfCnpj: String,
+                                        codigo: String,
+                                        codigoDigito: String,
+                                        agencia: String,
+                                        agenciaDigito: String
+                                    },
+                                    parcelas: [{
+                                        numero: Number,
+                                        valor: Number,
+                                        vencimento: Date,
+                                        nossoNumero: String
+                                    }]
+                                }
+                            },
+                            {
+                                name: 'sicredi',
+                                value: {
+                                    dataImpressao: Date,
+                                    dataGeracaoBoleto: Date,
+                                    numeroDocumento: String,
+                                    especie: String,
+                                    carteira: String,
+                                    numeroConvenio: String,
+                                    pagador: {
+                                        nome: String,
+                                        edereco: String,
+                                        cep: String,
+                                        cpfCnpj: String,
+                                    },
+                                    beneficiario: {
+                                        nome: String,
+                                        cpfCnpj: String,
+                                        codigo: String,
+                                        codigoDigito: String,
+                                        agencia: String,
+                                        agenciaDigito: String
+                                    },
+                                    parcelas: [{
+                                        numero: Number,
+                                        valor: Number,
+                                        vencimento: Date,
+                                        nossoNumero: String
+                                    }]
+                                }
+                            },
+                            {
+                                name: 'sicoob',
+                                value: {
+                                    dataImpressao: Date,
+                                    dataGeracaoBoleto: Date,
+                                    numeroDocumento: String,
+                                    especie: String,
+                                    carteira: String,
+                                    numeroConvenio: String,
+                                    pagador: {
+                                        nome: String,
+                                        edereco: String,
+                                        cep: String,
+                                        cpfCnpj: String,
+                                    },
+                                    beneficiario: {
+                                        nome: String,
+                                        cpfCnpj: String,
+                                        codigo: String,
+                                        codigoDigito: String,
+                                        agencia: String,
+                                        agenciaDigito: String
+                                    },
+                                    parcelas: [{
+                                        numero: Number,
+                                        valor: Number,
+                                        vencimento: Date,
+                                        nossoNumero: String
+                                    }]
+                                }
+                            }
+                        ]
+                    },
+                    return: {
+                        type: 'json',
+                        content: {
+                            url: { type: String, descricao: 'Url gerada para baixar ou imprimir o boleto' },
+                            idBoleto: { type: String, descricao: 'Id gerado no payment para relacionamento com api cliente' },
+                            parcelas: {
+                                type: Array,
+                                descricao: 'Dados da parcela',
+                                content: {
+                                    numero: { type: Number, descricao: 'Número da parcela' },
+                                    nossoNumero: { type: String, descricao: 'Nosso número ja gerado com o dígito' },
+                                    valor: { type: Number, descricao: 'Valor da parcela' },
+                                    dataVencimento: { type: Date, descricao: 'Data vencimento da parcela' }
+                                }
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    ]
+}
