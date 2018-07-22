4a690a543bd4c30e190937391e92df5985a1f589
Gustavo <gustavomaritan@gmail.com>
2018-07-22 09:05:35 -0300

    aa

--- docs.js
+++ docs.js
@@ -1,11 +1,7 @@
 let doc = {
     api: {
         name: 'Payment',
-        url: { /* Parãmetro no commander para definir qual ambiente usar */
-            dev: 'http://localhost:11002',
-            homolog: '',
-            prod: ''
-        },
+        url: 'http://localhost:11002',
         prefix: 'api',
         headers: { authorization: { type: String, descricao: 'Chave gerada pelo admin', testValue: '123' } }
     },
@@ -15,7 +11,7 @@ let doc = {
             descricao: 'Métodos para geração de boletos',
             routes: [
                 {
-                    name: 'Gerar Boleto',
+                    name: 'Gerar',
                     descricao: 'Método responsavel por gerar boleto e retornar url para impressão ou download.',
                     method: 'POST',
                     url: 'boleto/:banco',
@@ -125,18 +121,24 @@ let doc = {
                     },
                     return: {
                         type: 'json',
-                        content: {
-                            url: { type: String, descricao: 'Url gerada para baixar ou imprimir o boleto' },
-                            idBoleto: { type: String, descricao: 'Id gerado no payment para relacionamento com api cliente' },
-                            parcelas: {
-                                type: Array,
-                                descricao: 'Dados da parcela',
-                                content: {
-                                    numero: { type: Number, descricao: 'Número da parcela' },
-                                    nossoNumero: { type: String, descricao: 'Nosso número ja gerado com o dígito' },
-                                    valor: { type: Number, descricao: 'Valor da parcela' },
-                                    dataVencimento: { type: Date, descricao: 'Data vencimento da parcela' }
+                        success: {
+                            type: 'json',
+                            status: 200,
+                            content: {
+                                url: { type: String, descricao: 'Url gerada para baixar ou imprimir o boleto' },
+                                idBoleto: { type: String, descricao: 'Id gerado no payment para relacionamento com api cliente' },
+                                parcelas: {
+                                    type: Array,
+                                    descricao: 'Dados da parcela',
+                                    content: {
+                                        numero: { type: Number, descricao: 'Número da parcela' },
+                                        nossoNumero: { type: String, descricao: 'Nosso número ja gerado com o dígito' },
+                                        valor: { type: Number, descricao: 'Valor da parcela' },
+                                        dataVencimento: { type: Date, descricao: 'Data vencimento da parcela' }
+                                    }
                                 }
+                            },
+                            error: {
                             }
                         }
                     }
