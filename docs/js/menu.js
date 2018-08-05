7cd69be4f4421fad21b4c3dcefb65a989e4c8f9a
Gustavo <gustavomaritan@gmail.com>
2018-08-05 15:25:27 -0300

    a

--- docs/js/menu.js
+++
@@ -1,8 +0,0 @@
-$(document).on('click', '.menu [data-item]', function () {
-    let elem = $('.corpo [data-opcao="' + $(this).attr('data-item') + '"]');
-    elem.find('.collapsible-header').trigger('click');
-
-    $('.corpo').animate({
-        scrollTop: elem.position().top
-    }, 1000);
-});
